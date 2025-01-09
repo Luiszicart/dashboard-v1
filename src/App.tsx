e8364ba7ed5b7a3ffa4c4940c15d220b56ddaa68
Luiszicart <expansion88@pm.me>
2025-01-09 14:49:20 -0300

    feat(App): restructured app, libs and dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from "react-redux";
+import { ThemeProvider } from "styled-components";
+import { store } from "./store/store";
+import theme from "./styles/theme";
+import GlobalStyle from "./styles/globalStyles";
+import Dashboard from "./components/templates/Dashboard/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Dashboard />
+      </ThemeProvider>
+    </Provider>
   );
 }
 
